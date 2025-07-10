e192d76798c80bce6df0527bd1bf8c4a3e654b69
Shayan Abdullah <tuhinafroza07@gmail.com>
2025-07-10 23:56:05 +0600

    done2

--- class 38 react/vite.config.js
+++ class 38 react/vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Simple-Dark-Light-Mode-React-project-1-/',
   plugins: [react()],
-});
-
+})
